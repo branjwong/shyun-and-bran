d7803f57188b4c4a1ce63e5cce170827a21c9d72
Brandon J Wong <branjwong@gmail.com>
2017-02-07 15:56:14 -0800

    actually add Sightseeing.elm

---
+++ src/views/Preferences/Sightseeing.elm
@@ -0,0 +1,32 @@
+module Preferences.Sightseeing exposing (view)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+import Model exposing (..)
+
+import ViewUtilities
+
+
+
+
+view : Model -> Html Msg
+view model = 
+    let 
+        itemImagePairs = List.sortWith ViewUtilities.sortPreferences
+            [ ("Sensoji", "")
+            , ("Yokohama", "")
+            , ("Akihabara", "")
+            , ("Akihabara Electric City", "")
+            , ("Ghibli Museum", "")
+            , ("Show Me Around", "")
+            ]
+        (items, images) = List.unzip itemImagePairs
+        msgs = List.map (AddPreference Sightseeing) items
+    in
+        div
+            [ class "container" ]
+            [ h2 [ style [ ( "text-align", "center" ) ] ] [ text "Change your sightseeing preferences" ]
+            , ViewUtilities.blocks model.preferences.sightseeing items msgs images
+            , ViewUtilities.gotoButton PreferencesRoot "Back to Preference Select"
+            ]
